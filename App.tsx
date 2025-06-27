960fbcc7b7c7a583170cf5e9f0da8c4f314745cd
thakur00007 <thakurtd01@gmail.com>
2025-06-27 19:26:25 +0530

    Add navigation and initial screens for password generation and login

--- App.tsx
+++ App.tsx
@@ -4,206 +4,68 @@
  *
  * @format
  */
-import Clipboard from '@react-native-clipboard/clipboard';
-import CheckBox from 'react-native-check-box';
-import Slider from '@react-native-community/slider';
-import React, {useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
+
+import * as React from 'react';
 import {
-  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  TextInput,
-  ToastAndroid,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+
+import GeneratePassword from './views/components/GeneratePassword';
+import Login from './views/components/Login';
+
+export  type RootStackParamList = {
+  GeneratePassword: undefined;
+  Login: undefined;
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
 
-  const [password, setPassword] = useState<string>('');
-  const [passwordLength, setPasswordLength] = useState<number>(8);
-  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
-  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
-
-  useEffect(() => {
-    generatePassword();
-  }, [passwordLength, includeSymbols, includeNumbers]);
-
-  const generatePassword = () => {
-    let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (includeNumbers) {
-      charset += '012345678901234567890123456789';
-    }
-    if (includeSymbols) {
-      charset += '!@#$%^&*()_+!@#$%^&*()_+';
-    }
-    let retVal = '';
-    for (let i = 0, n = charset.length; i < passwordLength; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    setPassword(retVal);
-  };
-
-  const copyToClipboard = () => {
-    Clipboard.setString(password);
-    ToastAndroid.show('Password copied to clipboard', ToastAndroid.SHORT);
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={[
-          backgroundStyle,
-          {
-            height: '100%',
-          },
-        ]}>
-        <View style={styles.sectionContainer}>
-          <Text
-            style={[
-              styles.sectionTitle,
-              {
-                color: isDarkMode ? Colors.white : Colors.black,
-                width: '100%',
-                flex: 1,
-                textAlign: 'center',
-              },
-            ]}>
-            Password Generator
-          </Text>
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
 
-          <Text
-            style={[
-              styles.sectionDescription,
-              {marginTop: 30, color: isDarkMode ? Colors.white : Colors.black},
-            ]}>
-            password
-          </Text>
+      <NavigationContainer>
 
-          <TextInput
-            style={[
-              styles.textBox,
-              {color: isDarkMode ? Colors.white : Colors.black},
-            ]}
-            placeholder="password"
-            readOnly={true}
-            value={password}
+        <Stack.Navigator initialRouteName='Login'>
+
+          <Stack.Screen
+            name="GeneratePassword"
+            component={GeneratePassword}
+            options={{ title: 'GeneratePassword' }}
+          >
+            {/* <GeneratePassword></GeneratePassword> */}
+            {/* <GeneratePassword ></GeneratePassword> */}
+            {/* <></> */}
+          </Stack.Screen>
+
+          <Stack.Screen 
+            name="Login"
+            component={Login}
           />
-          <Text
-            style={[
-              styles.sectionDescription,
-              {marginTop: 0, color: isDarkMode ? Colors.white : Colors.black},
-            ]}>
-            password length
-          </Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignContent: 'center',
-              // marginVertical: 10,
-            }}>
-            <Slider
-              style={{width: 280, height: 40}}
-              minimumValue={8}
-              maximumValue={72}
-              step={1}
-              value={passwordLength}
-              onValueChange={value => setPasswordLength(value)}
-            />
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-              }}>
-              <Text style={{color: isDarkMode ? Colors.white : Colors.black}}>
-                {passwordLength}
-              </Text>
-            </View>
-          </View>
 
-          <View
-            style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignContent: 'center',
-              marginVertical: 10,
-            }}>
-            <CheckBox
-              style={[
-                {
-                  flex: 1,
-                  padding: 5,
-                  paddingVertical: 10,
-                  marginHorizontal: 2,
-                  backgroundColor: 'gray',
-                  borderRadius: 2,
-                },
-                isDarkMode
-                  ? {backgroundColor: 'gray'}
-                  : {backgroundColor: Colors.lighter},
-              ]}
-              onClick={() => {
-                setIncludeNumbers(!includeNumbers);
-              }}
-              isChecked={includeNumbers}
-              leftText={'Include Numbers'}
-              leftTextStyle={{color: isDarkMode ? Colors.white : Colors.black}}
-            />
-            <CheckBox
-              style={[
-                {
-                  flex: 1,
-                  padding: 5,
-                  paddingVertical: 10,
-                  marginHorizontal: 2,
-                  backgroundColor: 'gray',
-                  borderRadius: 2,
-                },
-                isDarkMode
-                  ? {backgroundColor: 'gray'}
-                  : {backgroundColor: Colors.lighter},
-              ]}
-              onClick={() => {
-                setIncludeSymbols(!includeSymbols);
-              }}
-              isChecked={includeSymbols}
-              leftText={'Include Symbols'}
-              leftTextStyle={{color: isDarkMode ? Colors.white : Colors.black}}
-            />
-          </View>
-          <View
-            style={{
-              width: '100%',
-              marginVertical: 10,
-              marginTop: 20,
-            }}>
-            <Button
-              title="Copy to Clipboard"
-              onPress={() => copyToClipboard()}
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </Stack.Navigator>
+
+      </NavigationContainer> 
+      
+    // </SafeAreaView>
   );
 }
 
---
+++ views/components/Login.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Login() {
+  return (
+    
+    // login form
+    <View>
+      <Text>Login</Text>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({})
